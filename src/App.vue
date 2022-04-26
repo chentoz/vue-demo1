a77026e686fab4479aa0f1894dbe6d528641e34f
chentoz <492309597@qq.com>
2022-04-26 17:42:34 +0800

    props

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>To-Do list</h1>
+    <input type="checkbox" id="todo-item" checked="false" />
+    <label for="todo-item">My Todo Item</label>
+  </div>
+  <div>
+    <ul>
+      <li>
+        <to-do-item label="My Todo Iteml With Label"></to-do-item>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ToDoItem from "./components/ToDoItem.vue"
+
+export default {
+  name: 'app',
+  components: {
+    ToDoItem
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
