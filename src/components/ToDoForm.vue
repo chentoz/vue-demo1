7d6339def2bc07d1bdd89b3e99127ff7e8d8c6d1
chenyixin <492309597@qq.com>
2022-04-27 11:39:59 +0800

    css : global / scoped

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -1,16 +1,19 @@
 <template>
   <form @submit.prevent="onsubmit">
-    <label for="new-todo-input" @click="label_click">
-      what needs to be done
-    </label>
+    <h2 class="label-wrapper">
+      <label for="new-todo-input" class="label__lg" @click="label_click">
+        What needs to be done?
+      </label>
+    </h2>
     <input
       type="text"
       id="new-todo-input"
       name="new-todo"
       autocomplete="off"
-      v-model="label"
+      v-model.lazy.trim="label"
+      class="input__lg"
     />
-    <button type="submit">Add</button>
+    <button type="submit" class="btn btn__primary btn__lg">Add</button>
   </form>
 </template>
 
@@ -21,8 +24,12 @@ export default {
       this.label = "clicked!";
     },
     onsubmit() {
+      if (this.label === "") {
+        return;
+      }
       // console.log('form submitted, label value ', this.label)
       this.$emit("todo-added", this.label);
+      this.label = "";
     },
   },
   data() {
